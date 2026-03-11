6787c6efe49b74822eca530b63aa416bb3ef64b3
canyonharris88 <canyonharris@gmail.com>
2026-03-11 15:30:56 +0000

    Remove New Alert button from Scout Agent screen

--- app-features.js
+++ app-features.js
@@ -1805,10 +1805,6 @@ function renderAlerts() {
   `;
 
   const newAlertHtml = `
-    <button class="btn btn-secondary new-alert-btn" onclick="toggleNewAlertForm()" id="newAlertToggle">
-      <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><line x1="12" y1="5" x2="12" y2="19"/><line x1="5" y1="12" x2="19" y2="12"/></svg>
-      New Alert
-    </button>
     <div class="new-alert-form ${showNewAlertForm ? 'visible' : ''}" id="newAlertForm">
       <div class="form-group">
         <label class="form-label">County</label>
